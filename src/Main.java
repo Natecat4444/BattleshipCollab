7b592ec66a61177961925d35296a36a348aa0b99
padillac <shinimaninima@gmail.com>
2018-11-24 12:39:21 -0500

    added main game loop to Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,21 @@
  * Created by nathankaufman on 11/24/18.
  */
 public class Main {
+    public boolean gameOver;
+
     public static void main(String[] args) {
+        this.gameOver = false;
         Player Player1 = new Player();
-        Player Player2 = new Player();
+
+
+        //Player Player2 = new Player();
+
+        //MAIN GAME LOOP
+        while (!this.gameOver){
+          //WAIT FOR OTHER PLAYER
+
+          //TAKE TURN
+          
+        }
     }
 }
--- src/Player.java
+++ src/Player.java
@@ -27,4 +27,9 @@ public class Player {
       return grid;
     }
 
+
+    public void takeTurn(){
+      
+    }
+
 }
