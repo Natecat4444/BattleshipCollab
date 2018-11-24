553375b78029482e2a37af55ea19b247ca52c1d2
nathankaufman <nlk7665@rit.edu>
2018-11-24 12:45:32 -0500

    Added menu

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,40 @@
+import java.util.Scanner;
+
 /**
  * Created by nathankaufman on 11/24/18.
  */
 public class Main {
-    public boolean gameOver;
+    public static boolean gameOver;
+    private static Scanner scanner;
 
     public static void main(String[] args) {
-        this.gameOver = false;
+        scanner = new Scanner(System.in);
+        gameOver = false;
+        System.out.print("Please enter one or two computers: ");
+        int computers = scanner.nextInt();
+        if(computers == 1){
+            //function call
+        }
+        else if(computers == 2){
+            //function call
+        }
+        else {
+            System.out.println("Invalid input, get learnt");
+            main(null);
+        }
+
         Player Player1 = new Player();
 
 
         //Player Player2 = new Player();
 
         //MAIN GAME LOOP
-        while (!this.gameOver){
+        while (!gameOver){
           //WAIT FOR OTHER PLAYER
 
           //TAKE TURN
           
         }
+        System.exit(1);
     }
 }
