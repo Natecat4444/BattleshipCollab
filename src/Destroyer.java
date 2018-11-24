846a542fd1a9df939b3d598ac08823d42e94ea2b
nathankaufman <nlk7665@rit.edu>
2018-11-24 12:18:37 -0500

    Destroyer

--- src/Destroyer.java
+++ src/Destroyer.java
@@ -1,5 +1,37 @@
 /**
  * Created by nathankaufman on 11/24/18.
  */
-public class Destroyer {
+public class Destroyer implements Ship {
+    private int health;
+    private int cooldown;
+
+    public Destroyer(){
+        health = 2;
+        cooldown = 0;
+    }
+
+    @Override
+    public void fire() {
+        cooldown = 0;
+    }
+
+    @Override
+    public boolean canFire() {
+        return true;
+    }
+
+    @Override
+    public boolean isAlive() {
+        return health>0;
+    }
+
+    @Override
+    public void takeDamage() {
+        health = health-1;
+    }
+
+    @Override
+    public void reduceCooldown() {
+        cooldown = 0;
+    }
 }
