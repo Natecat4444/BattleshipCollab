96a90d4ed644129ec754042ece9b414971d03f05
padillac <shinimaninima@gmail.com>
2018-11-24 12:28:08 -0500

    added createGrid function to player class

--- src/Player.java
+++ src/Player.java
@@ -4,5 +4,27 @@ import java.util.ArrayList;
  * Created by nathankaufman on 11/24/18.
  */
 public class Player {
-    protected ArrayList<Square> grid;
+    protected ArrayList<ArrayList<Square>> layout; //The grid representing player's own ships
+    protected ArrayList<ArrayList<Square>> radar; //The grid representing opponents ships
+
+    public Player(){
+      layout = createGrid();
+      radar = createGrid();
+    }
+
+    /**
+    * Method to create a 25*25 grid of empty squares
+    */
+    public ArrayList<ArrayList<Square>> createGrid(){
+      ArrayList<ArrayList<Square>> grid = new ArrayList();
+      for (int i = 0; i < 25; i++;){
+        grid.add(new ArrayList<Square>());
+        for (int j = 0; j < 25; j++;){
+          grid.get(i).add(new Square);
+        }
+      }
+
+      return grid;
+    }
+
 }
--- src/Square.java
+++ src/Square.java
@@ -2,6 +2,7 @@
  * Created by nathankaufman on 11/24/18.
  */
 public class Square {
+
     public Square(){
 
     }
