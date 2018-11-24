af0613828e6b471135723f8c6515d2af7c5c9cb9
nathankaufman <nlk7665@rit.edu>
2018-11-24 12:27:35 -0500

    Cruiser

--- src/Cruiser.java
+++ src/Cruiser.java
@@ -1,5 +1,38 @@
 /**
  * Created by nathankaufman on 11/24/18.
  */
-public class Cruiser {
+public class Cruiser implements Ship {
+    int health;
+    int cooldown;
+
+    public Cruiser(){
+        health = 3;
+        cooldown = 0;
+    }
+    @Override
+    public void fire() {
+        if(canFire()){
+            cooldown = 2;
+        }
+    }
+
+    @Override
+    public boolean canFire() {
+        return cooldown == 0;
+    }
+
+    @Override
+    public boolean isAlive() {
+        return health>0;
+    }
+
+    @Override
+    public void takeDamage() {
+        health = health-1;
+    }
+
+    @Override
+    public void reduceCooldown() {
+        cooldown = cooldown-1;
+    }
 }
