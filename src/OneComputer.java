90c8a04a63cb062f2c5b4d06d0f91aa41a23fe12
nathankaufman <nlk7665@rit.edu>
2019-09-12 14:47:48 -0400

    Starting one computer handlingp2

--- src/OneComputer.java
+++ src/OneComputer.java
@@ -8,10 +8,16 @@ public class OneComputer extends Observable {
     private Player player1;
     private Player player2;
     private Player CurrentPlayer;
+    private boolean gameOver;
 
     public OneComputer(Player player1, Player player2){
         this.player1 = player1;
         this.player2 = player2;
+        gameOver = false;
+    }
+
+    private void setupGame(){
+
     }
 
 
