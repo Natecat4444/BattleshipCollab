25a8e5d5891fb91e71f46b9cd683d1a224963b0e
nathankaufman <nlk7665@rit.edu>
2019-09-12 14:43:13 -0400

    Starting one computer handling

--- src/OneComputer.java
+++ src/OneComputer.java
@@ -5,5 +5,14 @@ import java.util.Observable;
  * Created by nathankaufman on 9/12/19.
  */
 public class OneComputer extends Observable {
+    private Player player1;
+    private Player player2;
+    private Player CurrentPlayer;
+
+    public OneComputer(Player player1, Player player2){
+        this.player1 = player1;
+        this.player2 = player2;
+    }
+
 
 }
